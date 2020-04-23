7dd8c4e709980582f72f1e3d9fa63b4afc1fc982
Bogdan Mazoure <bogdan.mazoure@mail.mcgill.ca>
2020-04-23 13:27:54 -0400

    Added simple code for MC

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# batch_stationary_distribution
- Implementation of "Batch stationary distribution estimation"
+# Batch stationary distribution estimation
+ Implementation of "Batch stationary distribution estimation" [link](https://arxiv.org/abs/2003.00722).
+
+ The example MC seems to converge to the true stationary function after 100 epochs:
+ ```
+[Epoch 95] V: -0.132449, Tau(0): 0.003317, Tau(1):0.003726, Tau(2):0.006164
+[Epoch 96] V: -0.140039, Tau(0): 0.003322, Tau(1):0.003732, Tau(2):0.006174
+[Epoch 97] V: -0.147585, Tau(0): 0.003328, Tau(1):0.003739, Tau(2):0.006186
+[Epoch 98] V: -0.155087, Tau(0): 0.003335, Tau(1):0.003747, Tau(2):0.006200
+[Epoch 99] V: -0.162546, Tau(0): 0.003343, Tau(1):0.003756, Tau(2):0.006216
+```
---
+++ estimate_stationary_dist.py
@@ -0,0 +1,130 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import copy
+from collections import defaultdict
+
+"""
+Implementation by Bogdan Mazoure
+Paper: https://arxiv.org/abs/2003.00722
+"""
+
+class Tau(nn.Module):
+    def __init__(self,input_dim):
+        super().__init__()
+        self.input_dim = input_dim
+
+        self.net = nn.Sequential(
+            nn.Linear(self.input_dim,32),
+            nn.Tanh(),
+            nn.Linear(32,1),
+            nn.Softplus()
+        )
+    def forward(self,x):
+        return self.net(x)
+
+
+if __name__ == "__main__":
+    """
+    Simple test with a 3x3 T. Its stationary vector is (0.25,0.5,0.25),
+    so tau(0),tau(1),tau(2) / (tau(0)+tau(1)+tau(2)) should converge to this
+    """
+    P = np.array([[0.5,0.5,0],[0.25,0.5,0.25],[0,0.5,0.5]])
+    n = len(P)
+    X_t, X_tp1 = [], []
+
+    for _ in range(1000):
+        x_t = np.random.choice(range(n),size=1)[0]
+        x_tp1 = np.random.choice(range(n),size=1,p=P[x_t])
+        X_t.append([1 if x_t == j else 0 for j in range(n)])
+        X_tp1.append([1 if x_tp1 == j else 0 for j in range(n)])
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    X_t = torch.FloatTensor(np.array(X_t,dtype=float)).to(device)
+    X_tp1 = torch.FloatTensor(np.array(X_tp1,dtype=float)).to(device)
+
+    
+
+    tau = Tau(n)
+    tau_t = Tau(n)
+    vee = torch.rand(1, requires_grad=True ,device=device)
+    if device == 'cuda':
+        tau = tau.cuda()
+        tau_t = tau_t.cuda()
+
+    tau_t.load_state_dict(tau.state_dict())
+
+    opt_tau = torch.optim.Adam(tau.parameters(),lr=0.01)
+    opt_vee = torch.optim.Adam([vee],lr=0.01)
+    
+
+    for epoch in range(100):
+
+        alpha_t = 1. / np.sqrt(epoch+1)
+        lam = 10
+
+        tau_xt = tau(X_t)
+        tau_xtp1 = tau(X_tp1)
+        tau_t_xt = tau_t(X_t)
+        tau_t_xtp1 = tau_t(X_tp1)
+        v = vee
+
+        grad_theta_tau_xt, grad_theta_tau_xtp1 = defaultdict(list),defaultdict(list)
+
+        for i in range(len(X_t)):
+            opt_tau.zero_grad()
+            tau_xt.backward([torch.FloatTensor([[1] if i==j else [0] for j in range(len(tau_xt))]).to(device)],retain_graph=True)
+
+            for param in tau.named_parameters():
+                grad_theta_tau_xt[param[0]].append(param[1].grad.clone())
+
+            # grad_theta_tau_i = tau.net[0].weight.grad.data[0].clone()
+            # grad_theta_tau_xt.append(grad_theta_tau_i)
+
+            opt_tau.zero_grad()
+            tau_xtp1.backward([torch.FloatTensor([[1] if i==j else [0] for j in range(len(tau_xtp1))]).to(device)],retain_graph=True)
+            for param in tau.named_parameters():
+                grad_theta_tau_xtp1[param[0]].append(param[1].grad.clone())
+            # grad_theta_tau_i = tau.net[0].weight.grad.data[0].clone()
+            # grad_theta_tau_xtp1.append(grad_theta_tau_i)
+            
+        opt_tau.zero_grad()
+        opt_vee.zero_grad()
+
+        for param in tau.named_parameters():
+            """
+            grad_theta: n_batch x n_out x n_in (matrix)
+                        n_batch x n_out (bias)
+            """
+            grad_theta_tau_xt_MAT = torch.stack(grad_theta_tau_xt[param[0]])
+            grad_theta_tau_xtp1_MAT = torch.stack(grad_theta_tau_xtp1[param[0]])
+
+            """
+            Defined both gradients as in Eq.17
+            """
+
+            if len(grad_theta_tau_xt_MAT.shape) == 3: # Matrix
+                tiled_tau_xt =  tau_xt.repeat(grad_theta_tau_xt_MAT.shape[1],1,grad_theta_tau_xt_MAT.shape[2]).permute(1,0,2)
+                tiled_tau_xtp1 =  tau_xtp1.repeat(grad_theta_tau_xtp1_MAT.shape[1],1,grad_theta_tau_xtp1_MAT.shape[2]).permute(1,0,2)
+            else: # Bias
+                tiled_tau_xt =  tau_xt.repeat(1,grad_theta_tau_xt_MAT.shape[1])
+                tiled_tau_xtp1 =  tau_xtp1.repeat(1,grad_theta_tau_xtp1_MAT.shape[1])
+
+            grad_J_tau = (tiled_tau_xt * grad_theta_tau_xt_MAT).mean(0) - (1 - alpha_t) * (tiled_tau_xt * grad_theta_tau_xt_MAT).mean(0) - alpha_t * (tiled_tau_xtp1 * grad_theta_tau_xtp1_MAT).mean(0) + 2*lam*v*grad_theta_tau_xt_MAT.mean(0)
+            grad_J_v = - (2 * lam * (tau_xt.mean() - 1 - v))
+            param[1].grad = grad_J_tau
+            vee.grad = grad_J_v
+            
+
+        opt_tau.step()
+        opt_vee.step()
+
+        tau_t.load_state_dict(tau.state_dict())
+
+        p1 = tau(torch.FloatTensor([[1.,0.,0.]]).to(device)).detach().cpu().item()
+        p2 = tau(torch.FloatTensor([[0.,1.,0.]]).to(device)).detach().cpu().item()
+        p3 = tau(torch.FloatTensor([[0.,0.,1.]]).to(device)).detach().cpu().item()
+
+        print('[Epoch %d] V: %f, Tau(0): %f, Tau(1):%f, Tau(2):%f'%(epoch,vee.item(),p1,p2,p3) )
+
+    
